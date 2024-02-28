29a74a634266b3deaa6b92817b89e8f88e233af6
Bhautik Patel <thummarbhautik7045@gmail.com>
2024-02-28 19:29:09 +0530

    Using Provider Archived State

--- lib/fifth.dart
+++ lib/fifth.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 // class Tutorials5 extends StatelessWidget {
 //   const Tutorials5({super.key});
@@ -45,7 +45,7 @@ import 'package:flutter/material.dart';
 //   @override
 //   Widget build(BuildContext context) {
 //     // TODO: implement build
-    
+
 //     return Padding(
 //       padding: const EdgeInsets.symmetric(vertical: 90),
 //       child: Column(
@@ -55,7 +55,7 @@ import 'package:flutter/material.dart';
 //           ElevatedButton(onPressed: () => setState(() {
 //             print('The Value Is $counter');
 //             counter++;
-//           }), 
+//           }),
 //           child: Text('Clcik ME'),)
 //         ],
 //       ),
@@ -63,61 +63,123 @@ import 'package:flutter/material.dart';
 //   }
 // }
 
-class StateManager extends StatefulWidget{
-  StateManager({super.key});
-  @override
-  State<StateManager> createState() => _StateManagerState();
+// class StateManager extends StatefulWidget{
+//   StateManager({super.key});
+//   @override
+//   State<StateManager> createState() => _StateManagerState();
+// }
+
+// class _StateManagerState extends State<StateManager>{
+//  int count = 999;
+//  void incriment(){
+//   setState(() {
+//     count++;
+//   });
+//  }
+//  void decriment(){
+//  setState(() {
+//    count--;
+//  });
+//  }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Scaffold(
+//        appBar: AppBar(
+//           leading: const IconButton(
+//             icon: Icon(Icons.menu),
+//             tooltip: 'Navigation menu',
+//             onPressed: null,
+//           ),
+//           title: const Text('Example title'),
+//           actions: const [
+//             IconButton(
+//               icon: Icon(Icons.search),
+//               tooltip: 'Search',
+//               onPressed: null,
+//             ),
+//           ],
+//         ),
+
+//         body: Center(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               FloatingActionButton(onPressed: incriment,
+//               child: Text('+1',style: TextStyle(fontSize: 23,color: Colors.green),),),
+//               Padding(
+//                 padding: const EdgeInsets.all(20),
+//                 child: Text('$count',style: TextStyle(fontSize: 40),),
+//               ),
+//               FloatingActionButton(onPressed: decriment,
+//               child: Text('-1',style: TextStyle(fontSize: 23,color: Colors.red),),),
+//             ],
+//           ),
+//         ),
+//     );
+//   }
+//   }
+
+// Passing The State With Provider
+
+class ProviderState with ChangeNotifier {
+  int count = 0;
+  void incriment() {
+    count++;
+    notifyListeners();
+  }
+
+  void decriment() {
+    count--;
+    notifyListeners();
+  }
+
+  int get _currentcount => count;
 }
 
-class _StateManagerState extends State<StateManager>{
- int count = 999;
- void incriment(){
-  setState(() {
-    count++;
-  });
- }
- void decriment(){
- setState(() {
-   count--;
- });
- }
-
+class myAppone extends StatelessWidget {
+  const myAppone();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-       appBar: AppBar(
-          leading: const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          title: const Text('Example title'),
-          actions: const [
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            ),
-          ],
-        ),
-
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FloatingActionButton(onPressed: incriment,
-              child: Text('+1',style: TextStyle(fontSize: 23,color: Colors.green),),),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text('$count',style: TextStyle(fontSize: 40),),
-              ),
-              FloatingActionButton(onPressed: decriment,
-              child: Text('-1',style: TextStyle(fontSize: 23,color: Colors.red),),),
-            ],
-          ),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => ProviderState(),
+      child: const UIpage(),
     );
   }
+}
+
+class UIpage extends StatelessWidget {
+  const UIpage();
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<ProviderState>(context);
+    return Scaffold(
+        body: Center(
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      FloatingActionButton(
+        onPressed: () => counter.incriment(),
+        child: const Text(
+          '+1',
+          style: TextStyle(fontSize: 23, color: Colors.green),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          '${counter._currentcount}',
+          style: const TextStyle(fontSize: 40),
+        ),
+      ),
+      FloatingActionButton(
+        onPressed: () => counter.decriment(),
+        child: const Text(
+          '-1',
+          style: TextStyle(fontSize: 23, color: Colors.red),
+        ),
+      ),
+    ])));
   }
+}
